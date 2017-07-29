93beb2b86668f07d48289100db57af7b9594979d
bbarber <bbarber@nebraskaglobal.com>
2017-07-29 16:46:23 -0500

    Load sample image and print center pixel

--- Program.cs
+++ Program.cs
@@ -1,12 +1,33 @@
 ﻿using System;
+using ImageSharp;
+using System.IO;
 
 namespace BikePOVImageGen
 {
     class Program
     {
+        // Number samples around the image
+        int RESOLUTION = 360;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // This will eventually be passed in
+            var filename = "foo.gif";
+
+
+            var fileStream = new FileStream(filename, FileMode.Open);
+            var image = Image.Load(fileStream); 
+
+            var imageWidth = image.Width;
+            var imageHeight = image.Height;
+
+            var centerPt = new Point(imageWidth / 2, imageHeight / 2);
+
+
+            var centerPx = image.GetPixelReference(centerPt.X, centerPt.Y);
+
+            Console.WriteLine(centerPx);
+            Console.WriteLine("Done.");
         }
     }
 }
